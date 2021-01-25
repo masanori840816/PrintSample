d5dde2ed6bad5b2f35bbc6fabeaf1e176be8cdc2
Masui Masanori <masanori840816@gmail.com>
2021-01-26 01:50:07 +0900

    add DI

--- PrintSample/App.xaml.cs
+++ PrintSample/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using PrintSample.Main;
 
 namespace PrintSample
 {
@@ -12,7 +14,20 @@ namespace PrintSample
     {
         public void Start(object sender, StartupEventArgs e)
         {
-
+            var servicesProvider = BuildService();
+            using (servicesProvider as IDisposable)
+            {
+                var main = servicesProvider.GetRequiredService<MainWindow>();
+                main.Show();
+            }
+        }
+        private static IServiceProvider BuildService()
+        {
+            var services = new ServiceCollection();
+            services.AddScoped<MainWindow>();
+            //services.AddScoped<MainViewModel>();
+            //services.AddScoped<ISpreadsheetLoader, SpreadsheetLoader>();
+            return services.BuildServiceProvider();
         }
     }
 }
